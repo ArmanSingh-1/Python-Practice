3b74c31b3dab40a7e80b8d7975f969f6bbdaf3fa
ArmanSingh-1 <156684173+ArmanSingh-1@users.noreply.github.com>
2024-04-08 16:23:21 +0000

    017 Practice Question

---
+++ 017.py
@@ -0,0 +1,14 @@
+'''
+Exercise 7
+Calculate the midpoint of the segment with ends at the points: A = (2, 4), B = (-4, 6)
+and print result to the console as shown below.
+Expected result:
+The middle point: (-1.0, 5.0)
+'''
+x1 = 2
+y1 = 4
+x2 = -4
+y2 = 6
+x = (x1 + x2)/2
+y = (y1 + y2)/2
+print(f'The middle point: ({x}, {y})')
