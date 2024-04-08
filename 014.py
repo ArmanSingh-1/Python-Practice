535595e0fcb5cc4cca16fb3a9132cfae3e1b8236
ArmanSingh-1 <156684173+ArmanSingh-1@users.noreply.github.com>
2024-04-08 15:49:50 +0000

    014 Practice Question

---
+++ 014.py
@@ -0,0 +1,14 @@
+'''
+Exercise 4
+The arithmetic sequence is given with the following formula:
+an = 10 + 4n
+Calculate the sum of the first ten elements of this sequence. 
+Print the result to the console as shown below.
+Expected result:
+The sum of the first 10 elements in a sequence: 320.0
+'''
+n = 10
+a1 = 14
+a10 = 50
+sum = n/2 * (a1 + a10)
+print(f'The sum of the first {n} elements in a sequence: {sum:.1f}')
