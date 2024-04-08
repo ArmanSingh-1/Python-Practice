a813b32fc604ec6678448f0ade42b5179a741449
ArmanSingh-1 <156684173+ArmanSingh-1@users.noreply.github.com>
2024-04-08 15:43:50 +0000

    013 Practice Question

---
+++ 013.py
@@ -0,0 +1,13 @@
+'''
+Exercise 3
+Write a program that calculates the delta for the quadratic equation:
+3x^2 - 4x + 1 = 0
+Print the result to the console as shown below.
+Expected result:
+Delta: 4
+'''
+a = 3
+b = -4
+c = 1
+d = b**2 - 4*a*c
+print(f'Delta: {d}')
